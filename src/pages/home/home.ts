5718713bc07768bb36cfbb4192397d976f917648
Francisco <fco-javier123@hotmail.com>
2018-05-31 16:13:15 -0600

    Stripe

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Stripe } from '@ionic-native/stripe';
+
 
 @Component({
   selector: 'page-home',
@@ -7,7 +9,58 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+
+
+/*
+   card : any = {
+    number: '4242424242424242',
+    expMonth: 7,
+    expYear: 2017,
+    cvc: '408'
+   };
+   card = {
+    number: '4242424242424242',
+    expMonth: 12,
+    expYear: 2020,
+    cvc: '220'
+   };
+
+   */
+
+   cadena = {
+    tokeng : ''
+   }
+
+   card :any = {
+    number: '',
+    expMonth: '',
+    expYear: '',
+    cvc: ''
+   };
+
+   text:string = '';
+
+  constructor(public navCtrl: NavController, private stripe: Stripe) {
+
+    this.stripe.setPublishableKey('pk_test_NArsQrKiBXVVifDUTU0kl5Z2');
+
+  }
+
+  ionViewDidLoad(): void{
+
+    this.stripe.setPublishableKey('pk_test_NArsQrKiBXVVifDUTU0kl5Z2');
+  }
+  
+  stripeSend(): void{
+    
+    this.stripe.createCardToken(this.card)
+   .then(token => //console.log(token.id)
+    {
+      this.cadena.tokeng = token.id;
+      console.log(token.id);
+    }
+    )
+   .catch(error => console.error(error));
 
   }
 
